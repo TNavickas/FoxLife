19347b9ed11fae8c0aa4ec5ad4bfc9e6c1092fde
Chen <Marinette.Chen@ec.gc.ca>
2019-07-10 15:46:23 -0400

    raghavan magic

---
+++ fo/Assets/wrapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wrapper : MonoBehaviour
+{
+    public avatarModel avatar;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void callFoxFunc(GameObject item, string itemName)
+    {
+        avatar.useItem(item, itemName);
+    }
+}
