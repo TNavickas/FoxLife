faf6922a78c18e3dadb27e5884b6fa2f797d7855
Tautvydas Navickas <tautvydas.navickas@canada.ca>
2019-07-10 17:50:22 -0400

    remove unused projects

--- CitySelection/Assets/SelectedCityInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class SelectedCityInfo {
-    public static string CityName { get; set; }
-    public static string CityCode { get; set; }
-}
