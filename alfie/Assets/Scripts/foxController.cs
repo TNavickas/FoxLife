070b7768e98030ef608886928a032f9b6b3c83b7
Chen <Marinette.Chen@ec.gc.ca>
2019-07-10 13:29:14 -0400

    code & assets for accessory and pet animators

--- alfie/Assets/Scripts/foxController.cs
+++ alfie/Assets/Scripts/foxController.cs
@@ -2,17 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+
 public class foxController : MonoBehaviour
 {
+    private Animator foxAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foxAnimator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /* Every update, check the settings of the fox and update the parameters in the animator*/
+        foxAnimator.SetBool("isFood", check_if_food());
+        foxAnimator.SetBool("isHungry", check_if_hungry());
+        foxAnimator.SetBool("isClothed", check_if_clothed());
+        foxAnimator.SetBool("isContent", check_if_content());
+        foxAnimator.SetBool("isCold", check_if_cold());
+        foxAnimator.SetBool("isUmbrella", check_if_umbrella());
+        foxAnimator.SetBool("isWater", check_if_water());
+        foxAnimator.SetBool("isThirsty", check_if_thirsty());
+        foxAnimator.SetInteger("Happiness", get_happiness());
+
     }
+    //checks to see if food is on the screen, returns true if found
+    public bool check_if_food()
+    {
+        return false;
+    }
+    //if just ate food return true
+    public bool check_if_hungry()
+    {
+        return false;
+    }
+
+    //checks to see if thirsty: if just drank water return true
+    public bool check_if_thirsty()
+    {
+        return false;
+    }
+    //checks to see if there is clothing on the screen, returns true if found
+    public bool check_if_clothed()
+    {
+        return true;
+    }
+
+    //checks to see if the fox is content, returns true if so
+    public bool check_if_content()
+    {
+        return true;
+    }
+    //checks to see if the fox is cold, returns true if so
+    public bool check_if_cold()
+    {
+        return true;
+    }
+
+    public bool check_if_umbrella()
+    {
+        return false;
+    }
+
+    public bool check_if_water()
+    {
+        return false;
+    }
+
+    //returns the happiness of the fox
+    public int get_happiness()
+    {
+        return 100;
+    }
+
+
 }
